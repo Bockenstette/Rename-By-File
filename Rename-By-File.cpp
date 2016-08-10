596cf53c8aaebe96c8fcd9adef44d19650bbc3d8
Kevin Bockenstette <bockenstette_kevin@live.com>
2016-08-10 19:33:45 -0400

    Removed uneeded directory parse. Fixed arg handling. Added in final functionality...now for testing.

--- Rename-By-File.cpp
+++ Rename-By-File.cpp
@@ -5,47 +5,50 @@ using namespace std;
 
 int main(int argument_count, char* arguments[])
 {
-    if(argument_count != 3 || argument_count != 2)
+    if(argument_count > 4 || argument_count < 2)
     {
         help();
         return -1;
     }
 
-	_chdir(arguments[1]);
-
-	vector<dirent*> files;
-	parse_directory(arguments[1], &files);
-
+	//Get all name pairs from the text file
 	pairvec name_vector;
-	parse_name_file((argument_count == 2 ? arguments[2] : arguments[1]), name_vector);
+	parse_name_file((argument_count == 3 ? arguments[2] : arguments[1]), 
+		            (argument_count == 4 ? *(arguments[3]) : ' '), 
+		             name_vector);
 
-	for each (strpair name_pair in name_vector)
+	//Get first file from directory w/ files to be renamed
+	WIN32_FIND_DATA file;
+	HANDLE file_handle = FindFirstFile(arguments[1], &file);
+
+	try
 	{
-		//TODO: Search for file base on name_pair.first, then replace with name_pair.second while being extension agnostic
+		do
+		{
+			for (unsigned i = 0; i < name_vector.size(); i++)
+			{
+				//If the old name from the text file is within the current filename
+				//TODO: This will most likely run into problems with smaller file names, maybe handle better
+				if (strstr(file.cAlternateFileName, name_vector[i].first.c_str()) != NULL)
+				{
+					//Rename the current file then delete the current pair
+					rename(file.cAlternateFileName, name_vector[i].second.c_str());
+					name_vector.erase(name_vector.begin() + i);
+					break;
+				}
+			}
+		} while (FindNextFile(file_handle, &file) != 0);
+	}
+	catch (exception e)
+	{
+		//Outputting the DWORD GetLastError() returns as an error code is ugly as sin...
+		cout << e.what() << " || " << GetLastError();
 	}
 
     return 0;
 }
 
-void parse_directory(char* sDirectoryPath, vector<dirent*> *rFiles)
-{
-	DIR *pDirectory;
-	struct dirent *pEntry;
-
-	rFiles->clear();
-
-	if ((pDirectory = opendir(sDirectoryPath)) != NULL) 
-	{
-		/* print all the files and directories within directory */
-		while ((pEntry = readdir(pDirectory)) != NULL) 
-		{
-			rFiles->push_back(pEntry);
-		}
-		closedir(pDirectory);
-	}
-}
-
-void parse_name_file(char* file_path, pairvec &name_vector)
+void parse_name_file(char* file_path, char seperator, pairvec &name_vector)
 {
 	string current_line;
     fstream name_file;
@@ -61,8 +64,9 @@ void parse_name_file(char* file_path, pairvec &name_vector)
 	while(getline(name_file, current_line))
 	{
 		int name_seperator_location = 0;
-		current_line.find_first_of(" ", name_seperator_location);
+		current_line.find_first_of(seperator, name_seperator_location);
 
+		// "123 456" -> <"123", "456">
 		string old_name = current_line.substr(0, name_seperator_location);
 		string new_name = current_line.substr(name_seperator_location + 1, current_line.length() - name_seperator_location - 1);
 
@@ -70,14 +74,28 @@ void parse_name_file(char* file_path, pairvec &name_vector)
 	}
 }
 
+//Inserts at first instance of size being larger than the one after it
 void largefirst_insert(strpair pair, pairvec &name_vector)
 {
+	//first and second case
+	if (name_vector.size() == 0)
+	{
+		name_vector.push_back(pair);
+		return;
+	}
+	else if(name_vector.size() == 1)
+	{ 
+		pair.first > name_vector[0].first ? name_vector.push_back(pair) : name_vector.emplace_back(pair);
+		return;
+	}
+
 	pairvec::iterator it = name_vector.begin();
 	do
 	{
 		if (pair.first.length() >= (it + 1)->first.length())
 		{
 			name_vector.insert(it, pair);
+			break;
 		}
 	} while (++it != name_vector.end());
 }
--- rename-by-file.h
+++ rename-by-file.h
@@ -5,7 +5,6 @@ using namespace std;
 typedef map<string, string> Name2NameMap;
 
 int main(int argument_count, char* arguments[]);
-void parse_directory(char* directory_path, vector<dirent*> *files);
-void parse_name_file(char* file_path, pairvec &name_vector);
+void parse_name_file(char* file_path, char seperator, pairvec &name_vector);
 void largefirst_insert(strpair pair, pairvec &name_vector);
 void help();
